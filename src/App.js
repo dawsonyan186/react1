9230fa6bcfc1cb95a657f20e48eb29430706eeb4
dawsonyan186 <326814950@qq.com>
2018-01-27 23:17:31 +0800

    App 传一个函数给 TodoInput

--- src/App.js
+++ src/App.js
@@ -12,14 +12,17 @@ class App extends Component {
       todoList: [{title:'第一个代办'},{title:'第二个代办'},{title:'第三个代办'}]
     }
   }
-  render() {
+  addTodo(){
+    console.log('我得添加一个 todo 了')
+  }
+  render() { 
     let todos = this.state.todoList.map((item, index) => {
       return (<li><TodoItem todo={item} /></li>)
-    });
+    })
     return <div className="App">
       <h1>我的待办</h1>
       <div className="inputWrapper"></div>
-      <TodoInput content={this.state.newTodo} />
+      <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
       <ol>
       {todos}
       </ol>
