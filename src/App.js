4632b1761d1878de0796c1040f518b4790c1f3f3
dawsonyan186 <326814950@qq.com>
2018-01-28 10:48:13 +0800

    消除「不要直接修改 state」的警告

--- src/App.js
+++ src/App.js
@@ -66,8 +66,9 @@ class App extends Component {
     </div>;
   }
   onSignUp(user) {
-    this.state.user = user
-    this.setState(this.state)
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
   }
 }
 
